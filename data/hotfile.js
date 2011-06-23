0d9a740457c5077b228776a60116c2f4a21ba47b
dgrosso <dgrosso@gmail.com>
2011-06-23 12:36:05 -0700

    Adapted to add-on-sdk-1.0 API

--- data/hotfile.js
+++ data/hotfile.js
@@ -1,10 +1,2 @@
-// This content script sends header titles from the page to the add-on:
-/*var script = "var elements = document.querySelectorAll('h2 > span'); " +
-			 "for (var i = 0; i < elements.length; i++) { " +
-			 "  postMessage(elements[i].textContent) " +
-			 "}";*/
 var element = document.querySelector('#download_file tr:first-child td:first-child');
-if( element && element.innerHTML.search(/removed/) == -1 )
-	postMessage(true);
-else
-	postMessage(false);
+self.postMessage( element && (element.innerHTML.search(/removed/) == -1) );
